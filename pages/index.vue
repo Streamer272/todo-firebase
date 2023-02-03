892e11fe49138f645e70d02c48239b3767cbbdb3
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-02 19:25:48 -0800

    :zap: Adds deleting

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <div v-else>
     <h1>Hello {{ auth.currentUser.displayName }}!</h1>
     <ul>
-        <li v-for="todo in todos">{{ todo.name }}</li>
+        <li v-for="todo in todos">{{ todo.data().name }}<button @click="deleteTodo(todo)">Delete</button></li>
     </ul>
     <form @submit="submit">
         <input v-model="newTodo" placeholder="Name" required>
@@ -16,22 +16,21 @@
 </style>
 
 <script lang="ts" setup>
-import { collection, where, query, onSnapshot, addDoc } from "firebase/firestore"
+import { collection, where, query, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore"
 
 const loading = ref(true)
 const todos = ref([])
 const newTodo = ref("")
 const { auth, firestore } = useFirebase()
-const todosCollection =collection(firestore, "todos")
+const todosCollection = collection(firestore, "todos")
 const unsubscribe = onSnapshot(query(
     todosCollection,
     where("uid", "==", auth.currentUser.uid)
 ), (snapshot) => {
-    console.log("onSnapshot")
     loading.value = true
     todos.value = []
     snapshot.forEach((doc) => {
-        todos.value.push(doc.data())
+        todos.value.push(doc)
     })
     loading.value = false
 })
@@ -44,5 +43,9 @@ function submit(event) {
         starred: false,
         uid: auth.currentUser.uid
     })
+}
+
+function deleteTodo(todo) {
+    deleteDoc(doc(firestore, "todos", todo.id))
 }
 </script>
